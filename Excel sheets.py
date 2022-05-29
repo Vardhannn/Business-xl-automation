4a7040ad0b05c575154a95a1e4db560b076f1f49
Sri Vardhan <99092172+Vardhannn@users.noreply.github.com>
2022-05-29 12:51:06 +0530

    Update Excel sheets.py

--- Excel sheets.py
+++ Excel sheets.py
@@ -9,8 +9,8 @@ def work_book(filename):
     for row in range(2, sheet.max_row + 1):
         cell = sheet.cell(row, 3)
         corrected_price = cell.value * 0.9
-        corrected_price_cell = sheet.cell(row, 4)
-        corrected_price_cell.value = corrected_price
+        sheet["c1"] = "corrected price"
+        cell.value = corrected_price
 
     Values = Reference(sheet, min_row=2, max_row=4, min_col=3, max_col=3)
 
